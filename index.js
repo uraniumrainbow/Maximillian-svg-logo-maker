d6c89258c4d80a0f16a3a84a6acf17830ca17601
uraniumrainbow <uraniumrainbow@gmail.com>
2023-09-17 19:45:55 -0600

    working 9-5 (what a way to make a living)

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const questions = require('./lib/questions.js');
+const fileName = "./examples/logo.svg";
+const { Triangle, Square, Circle } = require("./lib/shapes");
 
-const fs =  require('fs');
 
-const { Triangle, Square, Circle } = require('./lib/shapes');
+function setShape(response) {
 
-function writeToFile(fileName, answer) {
-    let svgText = '';
-    svgText = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-    svgString += '<g>'
+    if (response.shape === 'Circle') {
+        let userShape = new Circle (response.shapeColor, response.text, response.textColor)
+        return userShape.render()
+    }
+
+    if (response.shape === 'Square') {
+        let userShape = new Square (response.shapeColor, response.text, response.textColor)
+        return userShape.render()
+    }
+
+    if (response.shape === 'Triangle') {
+        let userShape = new Triangle (response.shapeColor, response.text, response.textColor)
+        return userShape.render()
+    }
+};
+
+
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
 }
+
+function init() {
+    inquirer 
+    .prompt(questions)
+    .then((response) => {
+        createLogo(response);
+        })
+    .catch(err => {
+            console.log(err)
+        });
+}
+
+init();
